91b4d87fae9350185ddb41a56cc6fefb307e29f7
Samuel Kasiera <kasieras@gmail.com>
2018-08-26 21:06:15 +0300

    Added the hold and roll functions to each player

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,89 @@
-var player1, player2;
-
-function rollDice() {
-  var die1 = document.getElementById('die1');
-  var die2 = document.getElementById('die2');
-  var status =document.getElementById("status");
-  var d1 = Math.floor(Math.random() * 6) + 1;
-  var d2 = Math.floor(Math.random() * 6) + 1;
-  var diceTotal = d1 + d2;
-  die1.innerHTML = d1;
-  die2.innerHTML = d2;
-  status.innerHTML = "You rolled " + diceTotal + ".";
-  if (d1 == d2){
-    status.innerHTML += " DOUBLES!! You earned an extra roll!"
-  }
-  $('#output').show();
+function Die() {
+  this.dieValue = 0;
 }
- $(document).ready(function () {
-   $("#btn").click(rollDice);
-   $("#players").click(function functionName() {
-    $("div#setup").hide();
-    $("#btn").show();
-    player1 = $("#player1").val();
-    player2 = $("#player2").val();
 
-   });
- });
+Die.prototype.value = function() {
+  this.dieValue = Math.floor(Math.random() * (6)) + 1;
+  return this.dieValue;
+};
+
+function Player(name) {
+  this.name = name;
+  this.turnScore = 0;
+  this.totalScore = 0;
+  this.turn = false;
+}
+
+Player.prototype.roll = function() {
+  var die = new Die();
+  var rollResult = die.value()
+  if (rollResult !== 1) {
+    this.turnScore += rollResult;
+  } else {
+    this.turnScore = 0;
+    this.turn = false;
+  }
+};
+
+Player.prototype.hold = function() {
+  this.totalScore += this.turnScore;
+  this.turnScore = 0;
+  this.turn = false;
+};
+
+
+$(document).ready(function() {
+  var player1 = new Player ("player1");
+  player1.turn = true;
+  var player2 = new Player ("player2");
+  player2.turn = false;
+
+$("#player1roll").click(function() {
+  player1.roll();
+
+  var newTurnScore = player1.turnScore;
+  var newTotalScore = player1.totalScore;
+  $(".player1TotalScore").text(newTotalScore);
+  $(".player1TurnScore").text(newTurnScore);
+  if(player1.turn === false) {
+    player2.turn = true;
+    }
+});
+
+$("#player1hold").click(function() {
+  player1.hold();
+
+  var newTurnScore = 0;
+  var newTotalScore = player1.totalScore;
+  $(".player1TotalScore").text(newTotalScore);
+  $(".player1TurnScore").text(newTurnScore);
+  if(player1.turn === false) {
+    player2.turn = true;
+    }
+});
+
+
+$("#player2roll").click(function() {
+  player2.roll();
+
+  var newTurnScore = player2.turnScore;
+  var newTotalScore = player2.totalScore;
+  $(".player2TotalScore").text(newTotalScore);
+  $(".player2TurnScore").text(newTurnScore);
+  if(player2.turn === false) {
+    player1.turn = true;
+    }
+});
+
+$("#player2hold").click(function() {
+  player2.hold();
+
+  var newTurnScore = 0;
+  var newTotalScore = player2.totalScore;
+  $(".player2TotalScore").text(newTotalScore);
+  $(".player2TurnScore").text(newTurnScore);
+  if(player2.turn === false) {
+    player1.turn = true;
+    }
+});
+});
