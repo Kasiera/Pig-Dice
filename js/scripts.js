3a1318099777c28d06aa19b476f767fa1361cad4
Samuel Kasiera <kasieras@gmail.com>
2018-08-26 21:01:28 +0300

    Edited html indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+var player1, player2;
+
 function rollDice() {
   var die1 = document.getElementById('die1');
   var die2 = document.getElementById('die2');
@@ -7,8 +9,19 @@ function rollDice() {
   var diceTotal = d1 + d2;
   die1.innerHTML = d1;
   die2.innerHTML = d2;
-  status.innerHTML = "You rolled" + diceTotal + ".";
+  status.innerHTML = "You rolled " + diceTotal + ".";
   if (d1 == d2){
     status.innerHTML += " DOUBLES!! You earned an extra roll!"
   }
+  $('#output').show();
 }
+ $(document).ready(function () {
+   $("#btn").click(rollDice);
+   $("#players").click(function functionName() {
+    $("div#setup").hide();
+    $("#btn").show();
+    player1 = $("#player1").val();
+    player2 = $("#player2").val();
+
+   });
+ });
